daf317c6d0c9b0f8d3c09cf3fd802de44183ed6a
Nick Lalic <nick.lalic@gmail.com>
2016-07-20 13:55:47 -0700

    Initial commit

---
+++ process_json.py
@@ -0,0 +1,15 @@
+from gsfileman import included_files
+import json
+import ntpath
+
+
+fpaths = included_files('.', ['*.json'])
+js = dict()
+
+for fpath in fpaths:
+    f = open(fpath, 'r')
+    fname = ntpath.basename(fpath)[:-5]
+    js[fname] = json.loads(f.read())
+
+print("{0} files loaded".format(len(js)))
+
